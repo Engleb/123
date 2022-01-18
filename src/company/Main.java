669210324ecf1173292c375c5f3bdd6e85c3e18a
engleb <engleb.a.gleb.ryzhkin@gmail.com>
2022-01-18 17:47:41 +0200

    -normal1-

--- src/company/Main.java
+++ src/company/Main.java
@@ -1,8 +1,30 @@
 package company;
+import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	new Main().run();
+
+    }
+
+    private void run() {
+        int[] mass = fillMass(20);
+        int max = mass[0];
+        for (int i = 1; i < mass.length; i++) {
+            if (mass[i]>max) max = mass[i];
+
+        }
+        System.out.println(max);
+
+    }
+
+    private int[] fillMass(int n ) {
+        int[] mass = new int[n];
+        Random random = new Random();
+        for (int i = 0; i < n; i++) {
+            mass[i] = random.nextInt(100);
+        }
+        return mass;
     }
 }
